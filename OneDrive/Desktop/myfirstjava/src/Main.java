3ab1e69fa3fa128c358b22cb18c7c916782daa85
Salil Thakur <salilthakur09@gmail.com>
2024-03-30 00:29:17 +0530

    Main.java

--- OneDrive/Desktop/myfirstjava/src/Main.java
+++ OneDrive/Desktop/myfirstjava/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 /*public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -863,8 +862,22 @@ public class Main{
 
 
 
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+}*/
+
+
+
+public class Main{
+    public static void main(String[]args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
