3488434b4d6ae8d1daa56ddf448def61e1d860c0
Salil Thakur <156768025+salilthakur9@users.noreply.github.com>
2024-06-28 17:05:26 +0530

    june28-1

--- wow.cpp
+++ wow.cpp
@@ -2294,3 +2294,100 @@ int main(){
 
 
 
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+
+
+#include<iostream>
+using namespace std;
+class binary{
+        string s;
+        public:
+        void read(void);
+        void chk_bin(void);
+    };
+    void binary :: read(void){
+        cout<<"enter a binary number:"<<endl;
+        cin>>s;
+    }
+    void binary :: chk_bin(void){
+        for(int i=0;i< s.length();i++){
+            if(s.at(i)!='0' && s.at(i)!='1'){
+                cout<<"Incorrect binary format"<<endl;
+                exit(0);
+            }
+        }
+    }
+int main(){
+    binary b;
+    b.read();
+    b.chk_bin();
+
+    return 0;
+}
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"  ";
+        }
+        for(int j=i;j<n;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
