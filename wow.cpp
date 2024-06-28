9f582c1142b64e4a7539d300a8b7b7bd9fdf8b3f
Salil Thakur <156768025+salilthakur9@users.noreply.github.com>
2024-06-28 17:33:44 +0530

    june28-2

--- wow.cpp
+++ wow.cpp
@@ -2345,19 +2345,14 @@ int main(){
 
 
 
-#include<iostream>
+/*#include<iostream>
 using namespace std;
-class binary{
-        string s;
-        public:
-        void read(void);
-        void chk_bin(void);
-    };
-    void binary :: read(void){
+    string s;
+    void read(void){
         cout<<"enter a binary number:"<<endl;
         cin>>s;
     }
-    void binary :: chk_bin(void){
+    void chk_bin(void){
         for(int i=0;i< s.length();i++){
             if(s.at(i)!='0' && s.at(i)!='1'){
                 cout<<"Incorrect binary format"<<endl;
@@ -2365,13 +2360,29 @@ class binary{
             }
         }
     }
+    void ones_comp(void){
+        for(int i=0;i<s.length();i++){
+            if(s.at(i)=='0'){
+                s.at(i)='1';
+            }else if(s.at(i)=='1'){
+                s.at(i)='0';
+            }
+        }
+    }
+    void display(void){
+        for(int i=0;i<s.length();i++){
+        cout<<s.at(i);
+        }
+    }
 int main(){
-    binary b;
-    b.read();
-    b.chk_bin();
+    read();
+    chk_bin();
+    cout<<"your binary number in the form of ones compliment is: ";
+    ones_comp();
+    display();
 
     return 0;
-}
+}*/
 
 
 
