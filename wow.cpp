7d05d7e0ce78d752a977762fae49990c6e8b71c5
Salil Thakur <salilthakur09@gmail.com>
2024-07-14 19:13:54 +0530

    14july-3

--- wow.cpp
+++ wow.cpp
@@ -3746,3 +3746,43 @@ if we are inheriting B from A and C from B: [A-->B-->C]
 1. A is the base class for B and B is the base class for C.
 2. A-->B-->C is called inheritance path.
 */
+
+
+
+/*#include<iostream>
+using namespace std;
+class Base1{
+    protected:
+    int base1int;
+    public:
+    void set_base1int(int a){
+        base1int = a;
+    }
+};
+
+class Base2{
+    protected:
+    int base2int;
+    public:
+    void set_base2int(int a){
+        base2int = a;
+    }
+};
+
+class Derived : public Base1, public Base2{
+    public:
+    void show(){
+        cout<<"The value of base1 is: "<<base1int<<endl;
+        cout<<"The value of base2 is: "<<base2int<<endl;
+        cout<<"The sum of these values is :  "<<base1int+base2int<<endl;
+    }
+};
+
+int main(){
+    Derived salil;
+    salil.set_base1int(49);
+    salil.set_base2int(52);
+    salil.show();
+
+    return 0;
+}*/
