bcd43b5b884f89a5911a4a6206450443fde4cb4d
Salil Thakur <salilthakur09@gmail.com>
2024-07-17 01:18:41 +0530

    17july-2

--- wow.cpp
+++ wow.cpp
@@ -3995,3 +3995,32 @@ int main(){
 
 
 
+//write a cpp code to find the factorial of an input number by using recursion(using classes).
+//ans:
+
+/*#include<iostream>
+using namespace std;
+class factorial{
+    int n;
+    public:
+    int fact(int n){
+        if(n<=1){
+            return 1;
+        }else{
+            return n*fact(n-1);
+        }
+    }
+};
+int main(){
+    factorial f;
+    int number;
+    cout<<"enter the number: "<<endl;
+    cin>>number;
+
+    int result=f.fact(number);
+    cout<<"factorial of "<<number<<" is: "<<result<<endl;
+    return 0;
+}*/
+
+
+
