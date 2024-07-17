5d068d61c7437e34435e286f017f651aea9a6153
Salil Thakur <salilthakur09@gmail.com>
2024-07-17 12:17:29 +0530

    17july-4

--- just.js
+++ just.js
@@ -94,9 +94,31 @@ else{
 
 
 // false for empty string:
-if(""){
+/*if(""){
     console.log("true");
 }
 else{
     console.log("false");
+}*/
+
+
+
+/*function same_last_digit(num1, num2){
+    const lastdigit1=num1%10;
+    const lastdigit2=num2%10;
+
+    return lastdigit1===lastdigit2;
 }
+
+const num1=202;
+const num2=102;
+const result = same_last_digit(num1,num2);
+
+if (result) {
+    console.log(`${num1} and ${num2} have the same last digit.`);
+} else {
+    console.log(`${num1} and ${num2} do not have the same last digit.`);
+}*/
+
+
+
