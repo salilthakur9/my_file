2d443f52e892f44feed3cf8e95f5372c773528a6
Salil Thakur <salilthakur09@gmail.com>
2024-07-17 23:35:22 +0530

    17july

--- wow.cpp
+++ wow.cpp
@@ -4060,14 +4060,50 @@ int main() {
 
 
 
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 using namespace std;
 int main(){
     vector<int>myvector;
-    myvector.push_back(4999);
+    myvector.push_back(4999); //this line enters first element in empty vector.
     cout<<"first element: "<<myvector[0]<<endl;
-    myvector.push_back(99);
+    myvector.push_back(99); //this line enters second element in vector that contains one element.
     cout<<"element at second place is: "<<myvector.at(1);
+    
     return 0;
+}*/
+
+
+
+//swap the alternating indices of an array
+/*#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 }
+
+void swapAlternate(int arr[], int size){
+    for(int i=0;i<size;i+=2){
+        if(i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+
+int main(){
+    int even[8]={1,2,3,4,5,6,7,8};
+    int odd[7]={1,2,3,4,5,6,7};
+
+    swapAlternate(even, 8);
+    printArray(even, 8);
+
+    cout<<endl;
+
+    swapAlternate(odd, 7);
+    printArray(odd, 7);
+
+    return 0;
+}*/
