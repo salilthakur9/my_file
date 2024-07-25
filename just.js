01f6e1a9be110e3961c0f6966ab365e644a65683
Salil Thakur <salilthakur09@gmail.com>
2024-07-25 16:50:16 +0530

    25-7

--- just.js
+++ just.js
@@ -548,3 +548,14 @@ e=Math.random();
 console.log(e);*/
 
 
+// math.random() , it generates any number in the range of 0 to 1 but not 0 and nor 1.
+// therefore to hgenerate it in the range of : eg. 1 to 10 so we can write it as: [math.random()*10], for 1 to 10 ; [math.random()*5], for 1 to 5.
+
+let a= Math.random()*100;
+console.log(a);
+//similarly:
+let b=Math.random()*1000;
+console.log(b);
+// to generate a random number in btw 0 to 10:
+let c= Math.floor(Math.random()*10)+1;
+console.log(c);
