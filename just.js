862bf02760f889ddd8a7839d68a72e13fadd3b39
Salil Thakur <salilthakur09@gmail.com>
2024-08-31 12:00:03 +0530

    31-8

--- just.js
+++ just.js
@@ -869,3 +869,13 @@ console.log("hello");*/
 
 
 
+const fs = require("fs")
+fs.readFile("a.txt", "utf-8", function(err, data){
+    console.log(data);
+})
+console.log("hi there");
+let a=0;
+for(let i =0;i<1000000000;i++){
+    a++;
+}
+console.log("hello buddy");
