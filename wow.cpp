8e41c8098482b658be5d85753fbdb0ec195d7150
Salil Thakur <salilthakur09@gmail.com>
2024-09-05 16:33:49 +0530

    5-9

--- wow.cpp
+++ wow.cpp
@@ -4708,3 +4708,98 @@ int main(){
 }*/
 
 
+//How does the String class in the provided code implement and use the [] operator for indexing, and what are the effects of modifying a character in the string using this operator?
+
+
+
+
+
+
+/*#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class String {
+public:
+    String(const char* str = "") {
+        size = strlen(str);
+        data = new char[size + 1];
+        strcpy(data, str);
+    }
+
+    ~String() {
+        delete[] data;
+    }
+
+    char& operator[](int index) {
+        if (index >= size) throw out_of_range("Index out of range");
+        return data[index];
+    }
+
+    void display() const {
+        cout << data << endl;
+    }
+
+private:
+    char* data;
+    int size;
+};
+
+int main() {
+    String str("Prabhakar");
+
+    cout << "Original string: ";
+    str.display();
+
+    str[0] = 'p';
+
+    cout << "Modified string: ";
+    str.display();
+
+    return 0;
+}*/
+
+
+/* How does the matrix class in the provided code implemented and use 
+the [] operator for accessing matrix element and what are the effects of
+modifing the matrix element using this operators */
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Matrix {
+public:
+    Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
+        data_ = new int[rows * cols];
+    }
+
+    ~Matrix() {
+        delete[] data_;
+    }
+
+    int* operator[](int row) {
+        if (row >= rows_ || row < 0) throw out_of_range("Row index out of range");
+        return &data_[row * cols_];
+    }
+
+    const int* operator[](int row) const {
+        if (row >= rows_ || row < 0) throw out_of_range("Row index out of range");
+        return &data[row * cols_];
+    }
+
+    void display() const {
+        for (int i = 0; i < rows_; ++i) {
+            for (int j = 0; j < cols_; ++j) {
+                cout << data[i * cols_ + j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+private:
+    int rows_, cols_;
+    int* data;
+};
+
