e09bac5adda6bd13a6713be0d1bea79953426e99
Salil Thakur <salilthakur09@gmail.com>
2024-11-12 16:49:53 +0530

    1st ques

---
+++ practiceQues.cpp
@@ -0,0 +1,26 @@
+// Create a simple C++ program with a class Factorial that has a method calculate to compute the factorial of a given number. 
+//The method should take an integer as input and return the factorial as an integer.
+
+/*#include<iostream>
+using namespace std;
+class Factorial{
+    public:
+    int result=1;
+    int calculate(int n){
+        for(int i=1;i<=n;i++){
+            result *=i;
+        }
+        return result;
+    }
+};
+int main(){
+    Factorial f;
+    int number;
+    cin>>number;
+
+    cout<<"factorial of "<<number<<" is "<<f.calculate(number)<<endl;
+    return 0;
+}*/
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
